474cd66d7412a70a8d3094f902ec77436e8ecf05
Ranielli Montagna <raniellimontagna@hotmail.com>
2024-12-10 08:57:09 -0300

    feat: :sparkles: implement tab rotation

--- public/background.js
+++ public/background.js
@@ -1,47 +1,68 @@
 // Listen for messages from the popup
-chrome.runtime.onMessage.addListener((message, _, sendResponse) => {
-  console.log('Mensagem recebida:', message)
-
-  if (message.status && message.tabs) {
-    console.log('Status recebido:', message.status)
-    console.log('Tabs recebidas:', message.tabs)
-    rotateTabs(message.tabs)
+chrome.runtime.onMessage.addListener(async (message, _, sendResponse) => {
+  if (!message.status && message.tabs) {
+    sendResponse({ status: 'Rotation stopped' })
+    return
   }
 
-  sendResponse({ status: 'OK' })
+  const tabs = await createTabs(message.tabs)
+  rotateTabs(tabs)
 })
 
 /**
- * @typedef {Object} Tab
+ * @typedef {Object} RawTab
  * @property {string} name
  * @property {string} url
  * @property {string} interval
  */
 
 /**
- *
- * @param {Array} tabs
+ * @description Create new tabs based on the tabs array
+ * @param {Array<RawTab>} tabs
  */
-async function rotateTabs(tabs) {
-  // Array to store the tab ids
+async function createTabs(tabs) {
+  // Array to store the tab ids with the time interval
   const tabIds = []
 
-  // Create new tabs
-  tabs.forEach((tab) => {
-    chrome.tabs.create({ url: tab.url }, (tab) => {
-      console.log('Tab created:', tab.id)
-      tabIds.push(tab.id)
+  // Create new tabs and wait for all to be created
+  await Promise.all(
+    tabs.map((tab) => {
+      return new Promise((resolve) => {
+        chrome.tabs.create({ url: tab.url }, (createdTab) => {
+          tabIds.push({ id: createdTab.id, interval: tab.interval })
+          resolve()
+        })
+      })
     })
-  })
+  )
 
   //Remove other tabs
   chrome.tabs.query({}, (allTabs) => {
     allTabs.forEach((tab) => {
-      if (!tabIds.includes(tab.id)) {
+      if (!tabIds.find((tabId) => tabId.id === tab.id)) {
         chrome.tabs.remove(tab.id)
       }
     })
   })
 
-  //TODO: implement rotate tabs
+  return tabIds
+}
+
+/**
+ * @typedef {Object} Tab
+ * @property {number} id
+ * @property {string} interval
+ */
+
+/**
+ * @description Rotate between tabs based on the time interval
+ * @param {Array<Tab>} tabs
+ */
+function rotateTabs(tabs) {
+  let currentTab = 0
+
+  setInterval(() => {
+    chrome.tabs.update(tabs[currentTab].id, { active: true })
+    currentTab = currentTab === tabs.length - 1 ? 0 : currentTab + 1
+  }, tabs[currentTab].interval)
 }
